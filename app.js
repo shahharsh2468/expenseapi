67a53698ad098aa325449c53d5d8807789c8c80e
shahharsh2468 <shahharsh2468@gmail.com>
2023-07-13 01:51:25 +0530

    #1 node module

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 const express = require("express");
 const db = require("./database/conn");
 const bodyParser = require('body-parser');
@@ -9,17 +9,13 @@ const app = express();
 
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
-app.use(cors({
-  origin: 'http://127.0.0.1:8080',
-  methods: 'GET, POST, PUT, DELETE',
-  allowedHeaders: 'Content-Type',
-}));
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:8080/'); // Replace with the URL of your front-end application
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  });
+app.use(cors());
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:8080/'); // Replace with the URL of your front-end application
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//     next();
+//   });
 app.use(express.static('public'));
 app.use(express.json());
 
